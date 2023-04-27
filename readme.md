343f045a23803f38a243526f419892f2080baa22
Royal6969 <sergiodiazcampos@gmail.com>
2023-04-27 16:42:14 +0200

    updating the ProductDetails page to use the redux query

--- readme.md
+++ readme.md
@@ -31,6 +31,7 @@
   - [3.1. Añadir Redux y Redux Toolkit](#31-añadir-redux-y-redux-toolkit)
   - [3.2. Añadir una consulta para buscar los productos](#32-añadir-una-consulta-para-buscar-los-productos)
   - [3.3. LLamar al Slice para buscar los productos](#33-llamar-al-slice-para-buscar-los-productos)
+  - [3.4. Cargar los detalles de un producto](#34-cargar-los-detalles-de-un-producto)
 - [Webgrafía y Enlaces de Interés](#webgrafía-y-enlaces-de-interés)
     - [1. What is the meaning of the "at" (@) prefix on npm packages?](#1-what-is-the-meaning-of-the-at--prefix-on-npm-packages)
     - [2. Bootstrap components](#2-bootstrap-components)
@@ -993,6 +994,99 @@ Si ahora ejecutamos la aplicación, mientras se cargan los rpoductos, podremos v
 
 ![](./img/17.png)
 ![](./img/18.png)
+
+## 3.4. Cargar los detalles de un producto
+
+Continuando por donde íbamos, habíamos colocado una plantilla de Bootstrap para la página de los detalles del producto, y teníamos hecha y configurada una redux query para ello. Vamos a usarla y a ver cómo funciona, por lo tanto, vamos a la págian de *ProductDetails.tsx*
+
+```tsx
+function ProductDetails() {
+  // the fact that we will be using useParams() hook here is because if you examine the product card when we're passing the details,
+  // like we're passing the product.id we have to catch it
+  // but one thing you should remember is where we have defined the route inside our App, we use the variable with the name :productId
+  // so that name must match with the name that we're using here to retrieve the parameters
+  const { productId } = useParams();
+  // now we have to use our second redux query to fetch the specific product
+  const { data, isLoading } = useGetProductByIdQuery(productId);
+  // console.log(data);
+
+  const navigate = useNavigate(); // to back to home button functionality
+
+  // to control the isLoading, instead use another flag like we did in ProductList, now we're going to use a conditional rendering
+  return (
+    <div className="container pt-4 pt-md-5">
+
+      {/* conditional rendering for isLoading */}
+      {!isLoading 
+        // if it's not loading then we want to display all the product details
+        ? (
+          <div className="row">
+            <div className="col-7">
+              <h2 className="text-warning">
+                { data.result?.name }
+              </h2>
+              
+              <span>
+                <span className="badge text-bg-dark pt-2" style={{ height: "40px", fontSize: "20px" }}>
+                  { data.result?.category }
+                </span>
+              </span>
+
+              <span>
+                <span className="badge text-bg-light pt-2" style={{ height: "40px", fontSize: "20px" }}>
+                  { data.result?.tag }
+                </span>
+              </span>
+              
+              <p style={{ fontSize: "20px" }} className="pt-2">
+                { data.result?.description }
+              </p>
+              
+              <span className="h3">
+                { data.result?.price }€  
+              </span> &nbsp;&nbsp;&nbsp;
+              
+              <span className="pb-2  p-3" style={{ border: "1px solid #333", borderRadius: "30px" }}>
+                <i className="bi bi-dash p-1" style={{ fontSize: "25px", cursor: "pointer" }}></i>
+                <span className="h3 mt-3 px-3">XX</span>
+                <i className="bi bi-plus p-1" style={{ fontSize: "25px", cursor: "pointer" }}></i>
+              </span>
+              
+              <div className="row pt-4">
+                <div className="col-5">
+                  <button className="btn btn-warning form-control">
+                    Add to Cart
+                  </button>
+                </div>
+
+                <div className="col-5 ">
+                  <button className="btn btn-secondary form-control" onClick={() => navigate(-1)}> {/* this is like the history.go(-1) */}
+                    Back to Home
+                  </button>
+                </div>
+              </div>
+            </div>
+            
+            <div className="col-5">
+              <img src={ data.result.image } width="100%" style={{ borderRadius: "50%" }} alt="No content" />
+            </div>
+          </div>
+        )
+        // else we want to display a <div> with the text of loading
+        : (
+          <div className='d-flex justify-content-center' style={{ width: "100%" }}>
+            <div>Loading product details...</div>
+          </div>
+        )
+      }
+
+    </div>
+  )
+}
+```
+
+![](./img/19.png)
+![](./img/20.png)
 
 # Webgrafía y Enlaces de Interés
 
--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,56 +1,88 @@
 import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useGetProductByIdQuery } from '../APIs/ProductAPI';
 
 
 function ProductDetails() {
+  // the fact that we will be using useParams() hook here is because if you examine the product card when we're passing the details,
+  // like we're passing the product.id we have to catch it
+  // but one thing you should remember is where we have defined the route inside our App, we use the variable with the name :productId
+  // so that name must match with the name that we're using here to retrieve the parameters
+  const { productId } = useParams();
+  // now we have to use our second redux query to fetch the specific product
+  const { data, isLoading } = useGetProductByIdQuery(productId);
+  // console.log(data);
+
+  const navigate = useNavigate(); // to back to home button functionality
+
+  // to control the isLoading, instead use another flag like we did in ProductList, now we're going to use a conditional rendering
   return (
     <div className="container pt-4 pt-md-5">
-      <div className="row">
-        <div className="col-7">
-          <h2 className="text-warning">NAME</h2>
-          
-          <span>
-            <span className="badge text-bg-dark pt-2" style={{ height: "40px", fontSize: "20px" }}>
-              CATEGORY
-            </span>
-          </span>
 
-          <span>
-            <span className="badge text-bg-light pt-2" style={{ height: "40px", fontSize: "20px" }}>
-              SPECIAL TAG
-            </span>
-          </span>
-          
-          <p style={{ fontSize: "20px" }} className="pt-2">
-            DESCRIPTION
-          </p>
-          
-          <span className="h3">$10</span> &nbsp;&nbsp;&nbsp;
-          
-          <span className="pb-2  p-3" style={{ border: "1px solid #333", borderRadius: "30px" }}>
-            <i className="bi bi-dash p-1" style={{ fontSize: "25px", cursor: "pointer" }}></i>
-            <span className="h3 mt-3 px-3">XX</span>
-            <i className="bi bi-plus p-1" style={{ fontSize: "25px", cursor: "pointer" }}></i>
-          </span>
-          
-          <div className="row pt-4">
-            <div className="col-5">
-              <button className="btn btn-warning form-control">
-                Add to Cart
-              </button>
+      {/* conditional rendering for isLoading */}
+      {!isLoading 
+        // if it's not loading then we want to display all the product details
+        ? (
+          <div className="row">
+            <div className="col-7">
+              <h2 className="text-warning">
+                { data.result?.name }
+              </h2>
+              
+              <span>
+                <span className="badge text-bg-dark pt-2" style={{ height: "40px", fontSize: "20px" }}>
+                  { data.result?.category }
+                </span>
+              </span>
+
+              <span>
+                <span className="badge text-bg-light pt-2" style={{ height: "40px", fontSize: "20px" }}>
+                  { data.result?.tag }
+                </span>
+              </span>
+              
+              <p style={{ fontSize: "20px" }} className="pt-2">
+                { data.result?.description }
+              </p>
+              
+              <span className="h3">
+                { data.result?.price }€  
+              </span> &nbsp;&nbsp;&nbsp;
+              
+              <span className="pb-2  p-3" style={{ border: "1px solid #333", borderRadius: "30px" }}>
+                <i className="bi bi-dash p-1" style={{ fontSize: "25px", cursor: "pointer" }}></i>
+                <span className="h3 mt-3 px-3">XX</span>
+                <i className="bi bi-plus p-1" style={{ fontSize: "25px", cursor: "pointer" }}></i>
+              </span>
+              
+              <div className="row pt-4">
+                <div className="col-5">
+                  <button className="btn btn-warning form-control">
+                    Add to Cart
+                  </button>
+                </div>
+
+                <div className="col-5 ">
+                  <button className="btn btn-secondary form-control" onClick={() => navigate(-1)}> {/* this is like the history.go(-1) */}
+                    Back to Home
+                  </button>
+                </div>
+              </div>
             </div>
-
-            <div className="col-5 ">
-              <button className="btn btn-secondary form-control">
-                Back to Home
-              </button>
+            
+            <div className="col-5">
+              <img src={ data.result.image } width="100%" style={{ borderRadius: "50%" }} alt="No content" />
             </div>
           </div>
-        </div>
-        
-        <div className="col-5">
-          <img src="https://via.placeholder.com/150" width="100%" style={{ borderRadius: "50%" }} alt="No content" />
-        </div>
-      </div>
+        )
+        // else we want to display a <div> with the text of loading
+        : (
+          <div className='d-flex justify-content-center' style={{ width: "100%" }}>
+            <div>Loading product details...</div>
+          </div>
+        )
+      }
+
     </div>
   )
 }
